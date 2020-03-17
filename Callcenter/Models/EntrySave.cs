6f2ee26ffab8ce1ab81f93a0bcceab84a943dbd9
Enis Belli <ixsen@mailbox.org>
2020-03-17 19:54:21 +0100

    Anpassung für AzureDB

--- Callcenter/Models/EntrySave.cs
+++ Callcenter/Models/EntrySave.cs
@@ -3,6 +3,7 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using Callcenter.Config;
 using Microsoft.Extensions.Options;
@@ -14,17 +15,23 @@ namespace Callcenter.Models
         //private readonly MongoClient client;
         //private readonly IMongoDatabase database;
         private readonly IMongoCollection<Entry> collection;
+
         public EntrySave(IOptions<MongoDbConf> options)
         {
             var mongoDbConf = options.Value;
-            var client = new MongoClient(mongoDbConf.Connection);
-            var database = client.GetDatabase(mongoDbConf.DbName);
+            
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(mongoDbConf.Connection));
+            settings.SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
+            var mongoClient = new MongoClient(settings);
+            
+            var database = mongoClient.GetDatabase(mongoDbConf.DbName);
             collection = database.GetCollection<Entry>("requests");
         }
+
         public List<Entry> GetAll() => collection.Find(e => true).ToList();
         public List<Entry> GetNoZip() => collection.Find(e => e.zip == "00000").ToList();
 
-        internal void Remove(ObjectId id)=> collection.DeleteOne(e => e.id == id);
+        internal void Remove(ObjectId id) => collection.DeleteOne(e => e.id == id);
 
         internal void Add(Entry entry)
         {
@@ -32,6 +39,7 @@ namespace Callcenter.Models
             {
                 entry.id = MongoDB.Bson.ObjectId.GenerateNewId();
             }
+
             collection.InsertOne(entry);
         }
 
@@ -42,11 +50,12 @@ namespace Callcenter.Models
 
 
         public static Random random = new Random();
+
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[random.Next(s.Length)]).ToArray());
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
