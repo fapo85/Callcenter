623c6f2436223c185c60212e3e86816a36a75a7c
Fabian Spottog <fabian@spottog.de>
2020-03-17 14:15:15 +0100

    objectid

---
+++ Callcenter/Models/MongoExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Callcenter.Models
+{
+    public static class MongoExtensions
+    {
+    }
+}
